9975ec47884a122512d3da1ed44865eaf0b82a7b
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2021-11-28 02:16:11 +0300

    Feat: add new component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
-  <app-BL />
-  <app-germeyer />
-  <app-HL />
-  <app-hurwitz />
-  <app-optimistic />
-  <app-p-critreria />
-  <app-savage />
-  <app-wald />
+  <app-matrix></app-matrix>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import AppBL from "./components/AppBL.vue";
-import AppGermeyer from "./components/AppGermeyer.vue";
-import AppHL from "./components/AppHL.vue";
-import AppHurwitz from "./components/AppHurwitz.vue";
-import AppOptimistic from "./components/AppOptimistic.vue";
-import AppPCritreria from "./components/AppPCritreria.vue";
-import AppSavage from "./components/AppSavage.vue";
-import AppWald from "./components/AppWald.vue";
+
+import AppMatrix from "./components/AppMatrix.vue";
+
 
 export default defineComponent({
   name: "App",
   components: {
-    AppBL,
-    AppGermeyer,
-    AppHL,
-    AppHurwitz,
-    AppOptimistic,
-    AppPCritreria,
-    AppSavage,
-    AppWald,
+    AppMatrix,
   },
 });
 </script>
