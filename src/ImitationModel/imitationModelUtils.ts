ebee08da36960af48b96d73ff611be3158c7d0c4
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2021-11-28 23:17:37 +0300

    Fix: remove algorithm bug and decompose

---
+++ src/ImitationModel/imitationModelUtils.ts
@@ -0,0 +1,85 @@
+import { calcPenalty, PeriodResultsFn } from "@/types";
+
+const calcUnderprodPenalty: calcPenalty = (unproducedItems, penalty) =>
+  unproducedItems * penalty;
+const calcOverprodPenalty: calcPenalty = (overproducedItems, penalty) =>
+  overproducedItems * penalty;
+
+const calcDailyDemand = (averageDailyDemand: number) => {
+  const delta = (averageDailyDemand * 10) / 100;
+  const currentDemand = Math.round(
+    getDemand(averageDailyDemand - delta, averageDailyDemand + delta)
+  );
+  return currentDemand;
+};
+const getDemand = (min: number, max: number) =>
+  Math.random() * (max - min) + min;
+
+export const calcPeriodResults: PeriodResultsFn = (
+  prodPeriod,
+  initialReserve,
+  averageDailyDemand,
+  prodPlan,
+  prodCosts,
+  overProdPen,
+  underProdPen,
+  expectedItemRevenue,
+  prodDelay,
+  prodPossibilities
+) => {
+  let totalProducesItems = 0;
+  let remainingItems = initialReserve;
+  let totalRevenue = 0;
+  let totalPenalties = 0;
+  let totalSoldItems = 0;
+  let totalDemand = 0;
+  const itemsArriveDays: number[] = [];
+
+  for (let i = 0; i < prodPeriod; i++) {
+    const dailyDemand = calcDailyDemand(averageDailyDemand);
+    totalDemand += dailyDemand;
+
+    if (itemsArriveDays[0] === i) {
+      itemsArriveDays.shift();
+      remainingItems += prodPossibilities;
+      totalProducesItems += prodPossibilities;
+    }
+
+    if (remainingItems > 0) {
+      remainingItems >= dailyDemand
+        ? ((totalRevenue += dailyDemand * expectedItemRevenue),
+          (totalSoldItems += dailyDemand))
+        : ((totalRevenue += remainingItems * expectedItemRevenue),
+          (totalSoldItems += remainingItems));
+    }
+    remainingItems -= dailyDemand;
+
+    if (remainingItems <= 0) {
+      totalPenalties += calcUnderprodPenalty(
+        Math.abs(remainingItems),
+        underProdPen
+      );
+    } else {
+      totalPenalties += calcOverprodPenalty(remainingItems, overProdPen);
+    }
+    if (
+      remainingItems <= averageDailyDemand &&
+      totalProducesItems <= prodPlan
+    ) {
+      itemsArriveDays.push(i + prodDelay);
+    }
+    remainingItems = remainingItems > 0 ? remainingItems : 0;
+  }
+  const totalCosts = totalProducesItems * prodCosts;
+  const income = totalRevenue - totalCosts - totalPenalties;
+  return {
+    income: income,
+    remainingItems: remainingItems,
+    totalProducesItems: totalProducesItems,
+    totalCosts: totalCosts,
+    totalSoldItems: totalSoldItems,
+    totalDemand: totalDemand,
+    totalRevenue: totalRevenue,
+    penalties: totalPenalties,
+  };
+};
--- src/ImitationModel/model.ts
+++ src/ImitationModel/model.ts
@@ -1,86 +1,16 @@
-import type { calcPenalty, PeriodResultsFn } from "@/types";
+import { calcPeriodResults } from "./imitationModelUtils";
 // Factory produces car engines. Production plan for month(30 days) - 200 engines
 
-const prodPlan = 200;
+const prodPlan = 400;
 const itemProdCost = 4000;
 const prodPeriod = 30;
+const prodDelay = 3; /* Minimum delay is 1, because we cant't produce new items in the same day*/
 const expectedItemRevenue = 5000;
 const underprodPenalty = expectedItemRevenue - itemProdCost;
 const overprodPenalty = 100;
-const initialReserve = 10;
-const prodPossibilities = Math.round(prodPlan / prodPeriod);
-const prodDelay = 2;
-
-const calcUnderprodPenalty: calcPenalty = (unproducedItems, penalty) =>
-  unproducedItems * penalty;
-const calcOverprodPenalty: calcPenalty = (overproducedItems, penalty) =>
-  overproducedItems * penalty;
+const initialReserve = 0;
+const prodPossibilities = Math.round(prodPlan / prodPeriod) * prodDelay;
 const averageDailyDemand = Math.round(prodPlan / prodPeriod);
-
-const getDemand = (min: number, max: number) =>
-  Math.random() * (max - min) + min;
-
-const calcDailyDemand = (averageDailyDemand: number) => {
-  const delta = (averageDailyDemand * 30) / 100;
-  const currentDemand = Math.round(
-    getDemand(averageDailyDemand - delta, averageDailyDemand + delta)
-  );
-  return currentDemand;
-};
-
-const calcPeriodResults: PeriodResultsFn = (
-  prodPeriod,
-  reserve,
-  averageDailyDemand,
-  plan,
-  costs,
-  overProdPen,
-  underProdPen,
-  expectedItemRevenue,
-  prodDelay,
-  prodPossibilities
-) => {
-  let totalProducesItems = 0;
-  let remainingItems = reserve;
-  let totalRevenue = 0;
-  let penalties = 0;
-  const arrivingItemsDays: number[] = [];
-  for (let i = 0; i < prodPeriod; i++) {
-    const dailyDemand = calcDailyDemand(averageDailyDemand);
-    if (arrivingItemsDays[0] === i) {
-      arrivingItemsDays.shift();
-      remainingItems += prodPossibilities;
-      totalProducesItems += prodPossibilities;
-    }
-    if (remainingItems > 0) {
-      remainingItems >= dailyDemand
-        ? (totalRevenue += dailyDemand * expectedItemRevenue)
-        : (totalRevenue += remainingItems * expectedItemRevenue);
-    }
-    remainingItems -= dailyDemand;
-
-    if (remainingItems < 0) {
-      penalties += calcUnderprodPenalty(Math.abs(remainingItems), underProdPen);
-    } else {
-      penalties += calcOverprodPenalty(remainingItems, overProdPen);
-    }
-    if (remainingItems < averageDailyDemand && totalProducesItems <= plan) {
-      arrivingItemsDays.push(i + prodDelay);
-
-      console.log(i, dailyDemand, arrivingItemsDays);
-    }
-  }
-  const totalCosts = totalProducesItems * costs;
-  const income = totalRevenue - totalCosts - penalties;
-  return {
-    income: income,
-    remainingItems: remainingItems,
-    totalProducesItems: totalProducesItems,
-    totalCosts: totalCosts,
-    totalRevenue: totalRevenue,
-    penalties: penalties,
-  };
-};
 
 //Usage
 
