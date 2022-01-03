46a793a146df734d52c1db5af19394b9b7c6874d
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-01-03 16:18:39 +0700

    Fix: remove unused features

--- src/types.ts
+++ src/types.ts
@@ -5,16 +5,3 @@ export interface BuilderData {
   matrix: number[][];
   probabilities: string;
 }
-export type calcPenalty = (itemProdCost: number, penalty: number) => number;
-export interface DailyResults {
-  dailyRevenue: number;
-  dailyDemand: number;
-}
-export interface PeriodResults {
-  income: number;
-  remainingItems: number;
-  totalCosts: number;
-  totalRevenue: number;
-  penalties: number;
-}
-export type PeriodResultsFn = (...args: number[]) => PeriodResults;
