7d8c2a363baef071615a56cb66d1f811d13395ba
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-01-03 19:59:08 +0700

    Refactor: rename components

---
+++ src/components/HodgesLehmann.vue
@@ -0,0 +1,80 @@
+<template>
+  <button
+    class="button-main"
+    type="button"
+    @click="calcHL(matrix, probabilities)"
+  >
+    calcHL
+  </button>
+  <div>{{ result }}</div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+import {
+  getLambda,
+  findMinInRows,
+  sumMathProbabilities,
+  prettifyOutput,
+  convertProbabilities,
+} from "../DecisonMakingMethods/utils";
+export default defineComponent({
+  data() {
+    return {
+      lambda: getLambda(),
+      result: "",
+    };
+  },
+  props: {
+    matrix: {
+      type: Object as PropType<number[][]>,
+      required: true,
+    },
+    probabilities: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    calcHL(matrix: number[][], probabilities: string, lambda = 0.5) {
+      const convertedProbabilities = convertProbabilities(probabilities);
+      const lambdaMathProbabilities = sumMathProbabilities(
+        matrix,
+        convertedProbabilities
+      ).map((value) => lambda * value);
+      const lambdaMinInRows = findMinInRows(matrix).map(
+        (value) => (1 - Number(lambda.toFixed(1))) * value
+      );
+      const options = lambdaMathProbabilities.map(
+        (value, i) => value + lambdaMinInRows[i]
+      );
+      const decision = Math.max(...options);
+      this.result = prettifyOutput(options, decision);
+    },
+  },
+});
+</script>
+<style scoped>
+.button-main {
+  height: 8%;
+  border: solid 1px;
+  background-color: none;
+  border-radius: 10px;
+  margin: 0;
+  padding: 3px;
+  width: auto;
+  overflow: visible;
+
+  background: transparent;
+
+  color: inherit;
+  font: inherit;
+
+  line-height: normal;
+}
+.button-main:hover {
+  background: rgb(248, 246, 246);
+  transition: background 0.1s ease-in;
+}
+</style>
